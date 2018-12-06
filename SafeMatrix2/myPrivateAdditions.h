1c9872d1377ea851f9ef70653397899f643960c5
Zahin Zaman <35665983+alvii147@users.noreply.github.com>
2018-12-05 21:54:49 -0500

    Delete myPrivateAdditions.h

--- SafeMatrix2/myPrivateAdditions.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYPRIVATEADDITIONS_H
-#define MYPRIVATEADDITIONS_H
-
-static float det(SafeMatrix& m);
-static SafeMatrix reduce(SafeMatrix& m, int i, int j);
-static bool identityTest(SafeMatrix& m);
-
-#endif
